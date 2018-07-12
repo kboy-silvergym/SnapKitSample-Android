df684b5d4399619d52a0fd822e40da73bddabb01
Kei Fujikawa <fujikei22funk@gmail.com>
2018-07-12 17:44:20 +0900

    change manifest file for snapchat login

--- app/src/main/java/net/kboy/snapkitclient/MainActivity.kt
+++ app/src/main/java/net/kboy/snapkitclient/MainActivity.kt
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
