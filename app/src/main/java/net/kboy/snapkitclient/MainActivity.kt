9d12ea52d2146836655daf5cc4db184aa23d8a10
Kei Fujikawa <fujikei22funk@gmail.com>
2018-07-13 19:20:55 +0900

    fetch user data using LoginKit

--- app/src/main/java/net/kboy/snapkitclient/MainActivity.kt
+++ app/src/main/java/net/kboy/snapkitclient/MainActivity.kt
@@ -1,15 +1,17 @@
 package net.kboy.snapkitclient
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
 import com.snapchat.kit.sdk.SnapLogin
+import com.snapchat.kit.sdk.SnapLogin.fetchUserData
 import com.snapchat.kit.sdk.core.controller.LoginStateController
+import com.snapchat.kit.sdk.core.models.UserDataResponse
+import com.snapchat.kit.sdk.login.networking.FetchUserDataCallback
 
-class MainActivity : AppCompatActivity(), LoginStateController.OnLoginStateChangedListener {
+class MainActivity : AppCompatActivity(), LoginStateController.OnLoginStateChangedListener, FetchUserDataCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,16 +29,11 @@ class MainActivity : AppCompatActivity(), LoginStateController.OnLoginStateChang
 
     }
 
-    fun goToNextView() {
-        val intent = Intent(this, LoginConfirmActivity::class.java)
-        startActivity(intent)
-    }
-
     // MARK : -  LoginStateController.OnLoginStateChangedListener
 
     override fun onLoginSucceeded() {
         print("login")
-        goToNextView()
+        fetchUserData()
     }
 
     override fun onLoginFailed() {
@@ -45,6 +42,31 @@ class MainActivity : AppCompatActivity(), LoginStateController.OnLoginStateChang
 
     override fun onLogout() {
         print("onLogout")
+    }
+
+    // MARK : - FetchUserDataCallback
+
+    override fun onSuccess(p0: UserDataResponse?) {
+        val me = p0!!.data.me
+        val name = me.displayName
+        val avatar = me.bitmojiData.avatar
+
+        // TODO: 画面遷移
+        goToNextView(name, avatar)
+    }
+
+    override fun onFailure(p0: Boolean, p1: Int) {
+
+    }
+
+    private fun goToNextView(name: String, avatar: String) {
+        val intent = Intent(this, LoginConfirmActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun fetchUserData() {
+        val query = "{me{bitmoji{avatar},displayName}}"
+        fetchUserData(this, query, null, this)
     }
 }
 
